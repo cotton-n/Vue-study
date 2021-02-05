df20a5d1f7d4e64c03e3028e2f3a73c9d2269eda
yeji <yeji9175@naver.com>
2021-02-05 14:56:58 +0900

    [Feat] App, TodoHeader 컴포넌트 스타일 지정
    배경색, 중앙 정렬, 마진, 테두리, 그림자 등 스타일 지정

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,9 +1,17 @@
 <template>
-  <div></div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
+</style>

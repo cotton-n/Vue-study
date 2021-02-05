7835ffef7a5ae41ff50f2d6548eff722d541fbdf
yeji <yeji9175@naver.com>
2021-02-05 18:42:39 +0900

    [Feat] TodoFooter 컴포넌트 구현
    TodoList 전체를 삭제하는 Clear All 버튼 생성
    clearTodo 메서드를 통해 전체 삭제 기능 구현

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,9 +1,30 @@
 <template>
-  <div></div>
+  <footer class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearTodo">Clear All</span>
+  </footer>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    clearTodo: function() {
+      localStorage.clear();
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllBtn {
+  color: #e20303;
+  display: block;
+}
+</style>

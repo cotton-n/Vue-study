606e9ef2de9f00e81aec872225ce668013422528
yeji <yeji9175@naver.com>
2021-02-05 15:09:45 +0900

    [Feat] TodoInput 컴포넌트 구현
    input, button 생성
    v-model 로 newTodoItem 과 연결
    addTodo 메서드 구현

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,9 +1,24 @@
 <template>
-  <div></div>
+  <div>
+      <input type="text" v-model="newTodoItem">
+      <button v-on:click="addTodo">add</button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data: function() {
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function() {
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.newTodoItem = '';
+        }
+    }
+};
 </script>
 
 <style></style>

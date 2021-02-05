d835e856e12e9272a48c68773f2c044714054ae5
yeji <yeji9175@naver.com>
2021-02-05 17:10:40 +0900

    [Feat] TodoList 컴포넌트 구현
    created 속성으로 localStorage에 저장된 데이터를 todoItems 배열에 저장
    v-for 를 통해 출력

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,9 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+        {{ todoItem }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      todoItems: [],
+    };
+  },
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  },
+};
 </script>
 
 <style></style>

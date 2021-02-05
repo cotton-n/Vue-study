efe799f19efcfa66613bd5b76f14a7cf71caef9a
yeji <yeji9175@naver.com>
2021-02-05 17:26:02 +0900

    [Feat] TodoList 컴포넌트 스타일 지정 및 삭제 기능 구현
    localStorage removeItem으로 데이터를 삭제하고 todoItems에서도 삭제하는 removeTodo 메서드 추가

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+      <li
+        v-for="(todoItem, index) in todoItems"
+        v-bind:key="index"
+        class="shadow"
+      >
         {{ todoItem }}
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
       </li>
     </ul>
   </div>
@@ -24,7 +31,46 @@ export default {
       }
     }
   },
+  methods: {
+    removeTodo: function(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1); // index 부터 1개 삭제
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>

8440ae6fcf962aeb240f92a2cf3d9848fee289ed
yeji <yeji9175@naver.com>
2021-02-05 15:58:56 +0900

    [Refactor] 함수 분리
    addTodo에 있던 input 초기화 로직을 clearInput 메서드로 분리

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,23 +1,26 @@
 <template>
   <div>
-      <input type="text" v-model="newTodoItem">
-      <button v-on:click="addTodo">add</button>
+    <input type="text" v-model="newTodoItem" />
+    <button v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
-    data: function() {
-        return {
-            newTodoItem: ""
-        }
+  data: function() {
+    return {
+      newTodoItem: '',
+    };
+  },
+  methods: {
+    addTodo: function() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
     },
-    methods: {
-        addTodo: function() {
-            localStorage.setItem(this.newTodoItem, this.newTodoItem);
-            this.newTodoItem = '';
-        }
-    }
+    clearInput: function() {
+      this.newTodoItem = '';
+    },
+  },
 };
 </script>
 

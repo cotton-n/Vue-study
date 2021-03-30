39ec5a120a8ae6a79f6b668a5587928f06af1152
yeji <yeji9175@naver.com>
2021-03-30 22:53:37 +0900

    [Feat] 라우터 설정

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,27 +1,21 @@
 <template>
   <div id="app">
-    hello world
+    <tool-bar></tool-bar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue';
+
 export default {
-  methods: {
-    fetchData() {
-      console.log('hello');
-      console.log('dfs');
-    },
-  },
+  components: { ToolBar },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ vue-news/src/routes/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import AskView from '../views/AskView.vue';
+import NewsView from '../views/NewsView.vue';
+import JobsView from '../views/JobsView.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: '/news',
+    },
+    {
+      // path: url 주소
+      path: '/news',
+      // component: url 주소로 갔을 때 표시될 컴포넌트
+      component: NewsView,
+    },
+    {
+      path: '/ask',
+      component: AskView,
+    },
+    {
+      path: '/jobs',
+      component: JobsView,
+    },
+  ],
+});

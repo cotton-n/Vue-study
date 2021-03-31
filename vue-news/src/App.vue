52677e5bdee788f731da569461399508948f4a4c
yeji <yeji9175@naver.com>
2021-04-01 02:27:01 +0900

    [Style] 라우터 트랜지션 추가

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -17,5 +19,15 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+
+/* Router Transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
 }
 </style>

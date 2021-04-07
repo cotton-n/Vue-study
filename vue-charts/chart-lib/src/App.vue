c8781f6b17cb23cc9c43d7072d8d0d38dd17aa06
yeji <yeji9175@naver.com>
2021-04-07 15:02:31 +0900

    [Chore] 개발환경설정

---
+++ vue-charts/chart-lib/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Chart.js</h1>
+    <bar-chart></bar-chart>
+    <line-chart></line-chart>
+  </div>
+</template>
+
+<script>
+import BarChart from './components/BarChart.vue';
+import LineChart from './components/LineChart.vue';
+
+export default {
+  components: {
+    BarChart,
+    LineChart,
+  },
+};
+</script>
+
+<style scoped></style>

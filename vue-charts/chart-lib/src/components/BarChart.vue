7fc521d6853d8f2e35cd77447ea67a28fcbb70e9
yeji <yeji9175@naver.com>
2021-04-07 15:03:46 +0900

    [Feat] 차트 라이브러리 모듈화
    - 차트 플러그인 생성 후 적용

---
+++ vue-charts/chart-lib/src/components/BarChart.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <canvas ref="chart"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    new this.$_Chart(this.$refs.chart, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 10, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ vue-charts/chart-lib/src/components/LineChart.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <canvas ref="chart"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'];
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1,
+        },
+      ],
+    };
+
+    new this.$_Chart(this.$refs.chart, {
+      type: 'line',
+      data: data,
+    });
+  },
+};
+</script>
+
+<style scoped></style>

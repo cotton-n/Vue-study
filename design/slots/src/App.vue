46b79c7a630e0c7386c1fe057e31f6af9e30ac0f
yeji <yeji9175@naver.com>
2021-05-26 21:47:00 +0900

    [Modify] item 내부 dom 수정
    slot 폴더 App 내의 각 item 별로 다양한 구조를 가지는 slot 적용

--- design/slots/src/App.vue
+++ design/slots/src/App.vue
@@ -2,10 +2,17 @@
   <div>
     <ul>
       <item>아이템 1</item>
-      <item>아이템 2</item>
-      <item>아이템 3</item>
-      <item>아이템 4</item>
-      <item>아이템 5</item>
+      <item>
+        아이템 2
+        <button>click me</button>
+      </item>
+      <item>
+        <div>아이템 3</div>
+        <img src="./assets/endgame.png" alt="어벤져스 엔드게임" width="300px" />
+      </item>
+      <item>
+        <div style="color: blue; font-size: 20px;">아이템 4</div>
+      </item>
     </ul>
   </div>
 </template>

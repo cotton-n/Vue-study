fc2bc173579829b9b32ffd7be5dddb80d47a66b6
yeji <yeji9175@naver.com>
2021-05-26 22:44:01 +0900

    [Feat] slot-scope 활용
    App에서 fetch-data로부터 전달받은 response, loading 데이터를 slot-scope을 활용하여 화면에 표시

--- design/renderless/src/App.vue
+++ design/renderless/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
-      <!-- ... -->
+      <div slot-scope="{ response, loading }">
+        <div v-if="!loading">
+          <p>name: {{ response.name }}</p>
+          <p>email: {{ response.email }}</p>
+        </div>
+        <div v-if="loading">
+          Loading..
+        </div>
+      </div>
     </fetch-data>
   </div>
 </template>
--- design/renderless/src/main.js
+++ design/renderless/src/main.js
@@ -5,18 +5,18 @@ Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-  // 1
-  render: function(createElement) {
-    return createElement(App);
-  },
-  // 2
-  render: function(h) {
-    return h(App);
-  },
-  // 3
-  render: h => {
-    return h(App);
-  },
-  // 4
-  render: h => h(App),
+  // // 1
+  // render: function(createElement) {
+  //   return createElement(App);
+  // },
+  // // 2
+  // render: function(h) {
+  //   return h(App);
+  // },
+  // // 3
+  // render: h => {
+  //   return h(App);
+  // },
+  // // 4
+  // render: h => h(App),
 }).$mount('#app');

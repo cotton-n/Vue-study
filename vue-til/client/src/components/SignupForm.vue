f48882e3833b1c723a75d6a8a448ab39d8f23592
yeji <yeji9175@naver.com>
2021-05-27 17:55:23 +0900

    [Feat] 회원가입 폼 생성
    - 데이터 바인딩 및 이벤트 연결

---
+++ vue-til/client/src/components/SignupForm.vue
@@ -0,0 +1,36 @@
+<template>
+	<form @submit.prevent="submitForm">
+		<div>
+			<label for="username">id: </label>
+			<input id="username" type="text" v-model="username" />
+		</div>
+		<div>
+			<label for="password">pw: </label>
+			<input id="password" type="text" v-model="password" />
+		</div>
+		<div>
+			<label for="nickname">nickname: </label>
+			<input id="nickname" type="text" v-model="nickname" />
+		</div>
+		<button type="submit">login</button>
+	</form>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			username: '',
+			password: '',
+			nickname: '',
+		};
+	},
+	methods: {
+		submitForm() {
+			console.log('폼 제출');
+		},
+	},
+};
+</script>
+
+<style scoped></style>

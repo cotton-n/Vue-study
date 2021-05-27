a670026c95aa1e18ed2f7d051811cde5b838af09
yeji <yeji9175@naver.com>
2021-05-27 16:56:27 +0900

    [Chore] vue 설정 추가
    웹 페이지명 변경

--- vue-til/client/vue.config.js
+++ vue-til/client/vue.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  devServer: {
-    // 화면에 eslint 에러가 표시되지 않도록하는 속성
-    overlay: false,
-    // npm run serve 했을 때 브라우저를 자동으로 띄워주는 속성
-    open: true,
-  },
+	devServer: {
+		// 화면에 eslint 에러가 표시되지 않도록하는 속성
+		overlay: false,
+		// npm run serve 했을 때 브라우저를 자동으로 띄워주는 속성
+		open: true,
+	},
+	// 웹 페이지명 변경
+	chainWebpack: config => {
+		config.plugin('html').tap(args => {
+			args[0].title = 'vue-til';
+			return args;
+		});
+	},
 };

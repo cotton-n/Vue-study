b3cdeca74302245ce6f0473150ccd9943560dbfd
yeji <yeji9175@naver.com>
2021-05-27 16:56:45 +0900

    [Feat] 로그인, 회원가입 페이지 라우터 설정

--- vue-til/client/src/routes/index.js
+++ vue-til/client/src/routes/index.js
@@ -1,6 +1,19 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import LoginPage from '@/views/LoginPage.vue';
+import SignupPage from '@/views/SignupPage.vue';
 
 Vue.use(VueRouter);
 
-export default new VueRouter();
+export default new VueRouter({
+	routes: [
+		{
+			path: '/login',
+			component: LoginPage,
+		},
+		{
+			path: '/signup',
+			component: SignupPage,
+		},
+	],
+});

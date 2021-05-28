c14f3b0b9f6ef76bb07a8be06a7284b216bc8b6d
yeji <yeji9175@naver.com>
2021-05-29 01:41:00 +0900

    [Style] CSS 링크 추가

--- vue-til/client/vue.config.js
+++ vue-til/client/vue.config.js
@@ -5,11 +5,4 @@ module.exports = {
 		// npm run serve 했을 때 브라우저를 자동으로 띄워주는 속성
 		open: true,
 	},
-	// 웹 페이지명 변경
-	chainWebpack: config => {
-		config.plugin('html').tap(args => {
-			args[0].title = 'vue-til';
-			return args;
-		});
-	},
 };

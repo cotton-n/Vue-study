17bd1ce69bbb386382b978b62f1990cd9fc01f83
yeji <yeji9175@naver.com>
2021-05-30 01:34:09 +0900

    [Feat] 데이터 포맷팅
    - 날짜 전역 필터 적용

--- vue-til/client/src/components/posts/PostListItem.vue
+++ vue-til/client/src/components/posts/PostListItem.vue
@@ -7,7 +7,7 @@
 			{{ postItem.contents }}
 		</div>
 		<div class="post-time">
-			{{ postItem.createdAt }}
+			{{ postItem.createdAt | formatDate }}
 			<i class="icon ion-md-create" @click="routeEditPage"></i>
 			<i class="icon ion-md-trash" @click="deleteItem"></i>
 		</div>
--- vue-til/client/src/main.js
+++ vue-til/client/src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from '@/routes';
 import store from '@/store';
+import { formatDate } from '@/utils/filters';
 
+Vue.filter('formatDate', formatDate);
 Vue.config.productionTip = false;
 
 new Vue({
